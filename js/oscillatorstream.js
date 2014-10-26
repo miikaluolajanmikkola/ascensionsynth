b169c37558264d563407011cdecc512297e6f945
Miika Luolajan-Mikkola <miika@luminescent-music.com>
2014-10-26 23:46:28 +0200

    Testing WAA Gain node

--- js/oscillatorstream.js
+++ js/oscillatorstream.js
@@ -3,21 +3,19 @@
 //Web Audio Context
 //https://developer.mozilla.org/en-US/docs/Web/API/AudioContext.createBufferSource
 
-
-
 var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 if( audioCtx == null ) alert('Your browser does not support Web Audio Api. Performance will be crap now, sorry');
 var channels = 2;
 // Create an empty two second stereo buffer at the
 // sample rate of the AudioContext
-var frameCount    = audioCtx.sampleRate * 2.0;
+var frameCount    = audioCtx.sampleRate;
 var myArrayBuffer = audioCtx.createBuffer(2, frameCount, audioCtx.sampleRate);
 
 
 function bufferWaveStream(samples) {
     
     //NOTE: This seems to not implement the Web Audio ScriptProcessorNode, which should be tested too.
-    console.log(samples);
+    //console.log(samples);
     for (var channel = 0; channel < channels; channel++) {
         
         // This gives us the actual ArrayBuffer that contains the data
@@ -29,29 +27,21 @@ function bufferWaveStream(samples) {
             nowBuffering[i] = samples[i];
         }
     }
+
     // Get an AudioBufferSourceNode.
     // This is the AudioNode to use when we want to play an AudioBuffer
     var source = audioCtx.createBufferSource();
+
+    //var gaynode = audioCtx.createGain();
+    source.connect(gaynode);
     // set the buffer in the AudioBufferSourceNode
     source.buffer = myArrayBuffer;
     // connect the AudioBufferSourceNode to the
     // destination so we can hear the sound
     source.connect(audioCtx.destination);
 
-    /* TODO: Hack gain properties from this progress and insert here upon
-    var gaynode = audioCtx.createGain();
-    gaynode.connect(audioCtx.destination);
-    gaynode.gain.value = 0.5 * this.mVolume/100.0;
-    
-    var node = audioCtx.createBufferSource();
-    node.buffer = this.mBuffer[id];
-    //node.gain.value = 0.5 * this.mVolume/100.0;
-    //node.connect(audioCtx.destination);
-    node.connect(gainNode);
-    //node.noteOn(0);
-    node.state = node.noteOn;
-    node.start(0);
-    */
+    //gaynode.gain.value = 0.01;
+
     // start the source playing
     source.start();
 }
