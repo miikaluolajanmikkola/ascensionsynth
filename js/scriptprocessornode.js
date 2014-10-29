9211a24ab9ecc8b5d7d545d43b64d7d58fe9ba63
Miika Luolajan-Mikkola <miika@luminescent-music.com>
2014-10-29 17:38:55 +0200

    Timing variables. Needs correct connection of additional node objects

--- js/scriptprocessornode.js
+++ js/scriptprocessornode.js
@@ -1,8 +1,50 @@
 //http://sriku.org/blog/2013/01/30/taming-the-scriptprocessornode/
 
+/**
+ * Examples of different Web Audio API nodes.
+ * @type {[type]}
+ *
+var pan        = AC.createPanner();
+var analyser   = audioCtx.createAnalyser();
+var distortion = audioCtx.createWaveShaper();
+var gainNode   = audioCtx.createGain();
+var biquadFilter = audioCtx.createBiquadFilter();
+var convolver  = audioCtx.createConvolver();
+
+lowpass.type = 'lowpass';
+lowpass.frequency.value = 1;
+lowpass.Q.value = 1;
+pan.setPosition(10, 5, 0);
+
+// Connect the nodes together
+source = audioCtx.createMediaStreamSource(stream);
+source.connect(analyser);
+analyser.connect(distortion);
+distortion.connect(biquadFilter);
+biquadFilter.connect(convolver);
+convolver.connect(gainNode);
+gainNode.connect(audioCtx.destination);
+
+/**/
+
 var AC = new ( window.AudioContext || window.webkitAudioContext)();
 var scriptNodes = {};
 
+var acAttack = 1;
+var acDecay = 0.1; // Only decay works, sounds more like a release to me.
+var acRelease = 5;
+
+
+/**
+ * Needs refactoring
+ * 
+ * @param  {[type]} t       [description]
+ * @param  {[type]} freq    [description]
+ * @param  {[type]} output  [description]
+ * @param  {[type]} samples [description]
+ * @return {[type]}         [description]
+ */
+/*
 function synthOsc(t, freq, output, samples) {
 
     freq = freq || 528.0;
@@ -25,35 +67,52 @@ function synthOsc(t, freq, output, samples) {
             var i, out;
             out = event.outputBuffer.getChannelData(0);
             for (i = fromSamp; i < toSamp; ++i, phi += dphi[i]) {
-                //out[i] = Math.sin(phi);
+                out[i] = Math.sin(phi);
             }
         };
     }()));
-}
 
-function synthEngineStream(t, freq, output, samples) {
+    osc.start(t);
+    osc.stop(t+10);
+
+}
+/**/
+function bufferOscStream(t, freq, output, samples) {
 
     freq = freq || 528.0;
-    t = Math.max(t || 0.0, AC.currentTime);
+    t = Math.max(t || 2, AC.currentTime);
     output = output || AC.destination;
-    var stopTime = t + 10.0;
+    var stopTime = t + acRelease;
 
-    var osc = AC.createOscillator();
+    var osc      = AC.createOscillator();
+
+    var gain = AC.createGain();
+    gain.gain.value = 0.0;
+    //gain.gain.setValueAtTime(0.25, t);
+    //gain.gain.setTargetAtTime(0.0, t, 2.0);
     
-    osc.frequency.value = freq;
 
+    osc.frequency.value = freq;
+    
     var gain = scriptWithStartStopTime(osc, output, t, stopTime, (function () {
-        var amplitude = 0.1;
-        var decay = Math.exp(- 1.0 / (2.0 * AC.sampleRate));
+
+        var amplitude = 0.4;
+
+        var decay = Math.exp(- 1.0 / (acDecay * AC.sampleRate));
+        
         return function (event, fromSamp, toSamp) {
             var i, inp, out;
             inp = event.inputBuffer.getChannelData(0);
             out = event.outputBuffer.getChannelData(0);
             for (i = fromSamp; i < toSamp; ++i, amplitude *= decay) {
+                //@todo: Let's call some coloring functions from waves.js here
                 out[i] = amplitude * inp[i];
+
             }
         };
     }()));
+
+    gain.connect(output || AC.destination);
 
     osc.start(t);
     osc.stop(stopTime);
@@ -79,7 +138,7 @@ function scriptWithStartStopTime(input, output, startTime, stopTime, handler) {
     console.assert(stopTime >= startTime);
 
     var kBufferLength = 512; // samples
-    var prepareAheadTime = 0.001; // seconds
+    var prepareAheadTime = 0.1; // seconds
 
     var startTime_samples = Math.floor(AC.sampleRate * startTime);
     var stopTime_samples = Math.ceil(AC.sampleRate * stopTime);
