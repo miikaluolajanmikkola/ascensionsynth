38b1213ab022518f7bd9032590d08ae14df15912
Miika Luolajan-Mikkola <miika@luminescent-music.com>
2014-10-29 17:33:53 +0200

    DSP timing, knob change implementation for EG release.

--- js/ascensionsynth.js
+++ js/ascensionsynth.js
@@ -1,9 +1,12 @@
 /**
  * Ascension Synthesizer
- * 3D Matrix Wavetable Designer / Microtonal Composer Concept
+ * Lo Shu Solfeggio Wavetable Designer / Microtonal Composer Concept
  * Version 0.5
  * 
- * Inspiration from:
+ * References, inspiration
+ * Stephen M. Phillips http://www.smphillips.8m.com/article-31.html
+ * Marshall Lefferts http://cosmometry.net/basics-of-the-music-system
+ * Curtis MacDonald / http://www.curtismacdonald.com/lo-shu-or-solfeggio-tonality-templates/
  *
  * Miika Kuisma / miikakuisma.com
  * Sakari Lehtonen / geokone.net
@@ -16,13 +19,12 @@
  * 
  */
 
-//var AC = new ( window.AudioContext || window.webkitAudioContext)();
-//var scriptNodes = {};
-
 //var preloadAudio = false;
 var gridMode = 'grid'; // 'solfeggios' || 'grid' = for the sake of development convenience
 
-var pluckedMax = 9;
+var soloMouseRunning = false;
+
+var pluckedMax = 6;
 var interval_length = 1000; 
 
 var seqStepLengthDefault = 8;
@@ -61,16 +63,22 @@ var oddFrequencies = [7.83, 63.05, 136.1];
 
 var solfeggios = dg1.concat(dg2, dg3, dg4, oddFrequencies);
 
+var holytree = [];
+var floweroflife = [];
+
 var grid = []; // Store all freqs here first
 var rowLength = 9;
+var gridStart = 1;
+var gridEnd   = rowLength;
 var waveCanvas = new Object(); // Contains wave data (floats or 255) assigned to freq cells { waveCanvas[freq] = samples }
 
 var sampleRate = 44100;
 var samples_length = sampleRate; // divide by 2 ???
 var samples = []; //new Float32Array(samples_length);
 
-var base = 9; //When subtraction of Z returns a negative number
-var zi = 9; //Z index
+var base = 9; //When subtraction of Z returns a negative number, these are ugly hacks
+var zi = 9; //Z index, growing by two on every x+yn
+
 var freq;
 var holyclass;
 
@@ -96,7 +104,17 @@ var csi = 0; //Current Sequence Index
 
 var currentKeyboard;
 
+var webSynth;
+
+
+
+
+
+
 /**
+ *	Iterate through Grid, create and bind wavedata to waveCanvas. Even if the use of Web Audio API becomes
+ *	exclusive, this might be useful for future User Wave generation, "export mp3" etc.
+ * 
  *  Fill the grid with raw waveform samples
  *  Atm overwrites previous collection of audio
  */
@@ -119,7 +137,7 @@ function createWaveGrid(waveform) {
 		break;
 
 		case "grid":
-			
+			//waveCanvas.clear();
 			for (i = 0; i < grid.length; i++) {
 				var fnparams = [grid[i], samples_length];
 				var fn = window[waveform];
@@ -136,26 +154,47 @@ function createWaveGrid(waveform) {
 
 
 /**
- *  Calls to Web Audio API ScriptProcessorNode
+ *  Calls to Web Audio API
  */
 function playTone(freq) {
 
-	// ScriptProcessorNode plays Oscillator as default wave until grid waves are created
+	// WEBSYNTH (SPN)
+	if ( webSynth) {
+		//console.log('websynth play');
+		//setTimeout(function(){ webSynth.play(freq); }, 1000);
+		//webSynth.play(freq);
+	}
+	// WAVE PROCESS BUFFER
 	if ( waveCanvas[freq] ) {
 		//Now we have samples to use SPN with our JS methods in waves.js
-		bufferWaveStream( waveCanvas[freq] );
+		//console.log('buffering samples');
+		bufferWaveStream( waveCanvas[freq]);
 	}
+	// SCRIPT PROCESSOR NODE
 	else {
-		// THIS is the cooler method with added time functions:
+		// ScriptProcessorNode plays Oscillator as default wave until grid waves are created
+		// The call below is the more promising method with added time functions:
 		// send waveCanvas[freq] (float array) as additional parameter?
-		synthEngineStream( 0.001, freq, output = null); 
+		// param output = AC.audioContext can be called third - it used to be defined "null" for some reason
+		//console.log('buffering osc');
+		bufferOscStream( 0.5, freq); 
 	}
 	return;
 
 }
 
+/**
+ * Parameter to be given to this function should be designed carefully!
+ * When multiple different tones are playing, it has to know the whole data structure
+ * 
+ */
+function stopTone() {
 
-/* * * ** *** *****  BEGIN UI  ***** *** ** * * */
+	//webSynth.stop();
+}
+
+
+/*****  BEGIN UI  ******/
 /**
  * Grid layout to HTML
  * 
@@ -171,16 +210,21 @@ function createSacredGridLayout() {
 	var ex = 0; //RGB color emphasis value
 
 	var html = '';
+
+	
+
 	for ( y = 9; y > 0; y-- ) {
 		for ( x = 1; x <= 9; x++ ) {	
 
-			//z = cos(x+y+z);
-			//alert(z);
+			var alpha, cos, tan;
 
+			//z = cos(x);
 			z = zi - x;
-			
 			if ( z < 1 )
 				z = base - (abs(z));
+			
+			//console.log(z);
+			
 			if (x == 0) x = '';
 			if (y == 0) y = '';
 			//if (z == 0) z = '';
@@ -201,7 +245,7 @@ function createSacredGridLayout() {
 			}
 			
 			holyclass = (solfeggios.indexOf(eval(freq)) == -1) ? '' : 'holy';	
-			html += '<div id="'+ freq +'" '+ rgb +' class="freqButton '+ holyclass +'"><div class="plucked"><span>'+ freq +'</span></div><span class="default_grid_freq_view">'+ freq +'</span></div>';
+			html += '<div id="'+ freq +'" '+ rgb +' class="freqButton '+ holyclass +'"><div class="plucked"><span class="noselect">'+ freq +'</span></div><span class="default_grid_freq_view noselect">'+ freq +'</span></div>';
 
 			if ( x == 9 ) {
 				zi = zi - 2;
@@ -211,10 +255,11 @@ function createSacredGridLayout() {
 			}
 		}
 	}
-	html += '</div><div id="bonus">';
+	html += '</div>';
+	html += '<div id="bonus">';
 		for (i = 0; i < oddFrequencies.length; i++) {
 			//rgb = 'style="background-color: #' + x + x + y + y + z + z + '" ';
-			html += '<div id="'+ oddFrequencies[i] +'" class="freqButton holy"><div class="plucked"><span>'+ oddFrequencies[i] +'</span></div><span class="default_grid_freq_view">'+ oddFrequencies[i] +'</span></div>';
+			html += '<div id="'+ oddFrequencies[i] +'" class="freqButton holy"><div class="plucked"><span class="noselect">'+ oddFrequencies[i] +'</span></div><span class="default_grid_freq_view class="noselect"">'+ oddFrequencies[i] +'</span></div>';
 			grid.push(oddFrequencies[i]);
 		}
 	html += '</div>';
@@ -302,47 +347,25 @@ function prepareSeqSelector() {
 	return markup;
 }
 
+function freqButtonEventHandle(freqButton) {
 
-
-/**
- * Let's run this stuff
- */
-$(document).ready(function () {
-	
-	$('#grid').append( createSacredGridLayout() );
-	$('#kbSelector').append( prepareKbSelector() );
-	$('#temperingSelector').append( prepareTemperingSelector() );
-	$('#waveSelector').append( prepareWaveSelector() );
-	$('#seqSelector').append( prepareSeqSelector() );
-	$('.plucked').hide();
-	
-	//createWaveGrid('sinewave');
-
-	//currentSequences
-	//Must be array to handle multiple seqs at once. 
-	currentKeyboard = $('#keyboardSelector option:selected').val();
-
-	$('#seq_start_stop').trigger('click');
-	
-	$('.freqButton').on('click', function() {
-		
-		var opa = 0.7;
-		var pluckedElem = $(this).find('.plucked');
+		var pluckedElem = $(freqButton).find('.plucked');
 		var ptv = $(pluckedElem).text(); // Plucked Text Value
 
 		playTone(ptv);
 
-		plucked.push(this);
+		soloMouseRunning = true;
+
 		var rgb = ptv;
-		
+		var opa = 0.7;
 		$(pluckedElem).show().css('opacity', opa);
-		$(this).find('.default_grid_freq_view').hide();
+		$(freqButton).find('.default_grid_freq_view').hide();
 
 		//Trails
 		for (i = 0; i < plucked.length; i++) {
 			var felem = $(plucked[plucked.length-i]).find('.plucked');
 			if ($(felem).text() != ptv)
-				$(felem).animate({'opacity' : opa-i/30}, 1);
+				$(felem).css('opacity', opa-i/30);
 			else
 				$(felem).css('opacity', opa);
 			rgb[0] +=  $(felem).text()[0];
@@ -354,17 +377,72 @@ $(document).ready(function () {
 			if ($(fadePlucked).text() != ptv) {
 				$(fadePlucked).find('.plucked').hide();
 				$(fadePlucked).find('.default_grid_freq_view').show();
+				plucked.shift();
 			}
-			plucked.shift();
 			//console.log(plucked);
 		}
+		plucked.push(freqButton);
 		//console.log(rgb);
-		//var colorSum = 
 		$('#lumisonos').css('background-color', '#' + rgb[1] + rgb[1] + rgb[2] + rgb[2] + rgb[0] + rgb[0]);
 		//$('body').css('background', 'radial-gradient(ellipse at center, rgba(65,74,91,1) 0%,rgba('+rgb[1]+rgb[1]+', '+rgb[2]+rgb[2]+', '+ rgb[0] + rgb[0] +',1) 100%)');
 		//$('#grid').css('border-color', '#0301' + rgb[1] + rgb[1]);
+		
+		return;
+}
+
+
+
+
+$(document).ready(function () {
+	
+	$('#grid').append( createSacredGridLayout() );
+	$('#kbSelector').append( prepareKbSelector() );
+	$('#temperingSelector').append( prepareTemperingSelector() );
+	$('#waveSelector').append( prepareWaveSelector() );
+	$('#seqSelector').append( prepareSeqSelector() );
+	$('.plucked').hide();
+	//setTimeout(function(){
+	$('.wsBtnWrap').each(function(){
+		var w = parseInt( $(this).find('input').attr('data-width'), 10);
+		var h = parseInt( $(this).find('input').attr('data-height'), 10);
+		$(this).css('width', w + 6 ).css('height', h + 6 );
+		//.find('input').attr('background-color', '#ff0000');
+		//doesn't affect canvas drawn color 
+	});
+	//}, 1000);
+	/**/
+
+	//createWaveGrid('sinewave');
+
+	$('.knob').knob();
+
+	//webSynth = new WebSynth();
+
+	//currentSequences
+	//Must be array to handle multiple seqs at once. 
+	currentKeyboard = $('#keyboardSelector option:selected').val();
+
+	//$('#seq_start_stop').trigger('click');
+	
+	$('.freqButton').on('mousedown', function() {
+		
+		freqButtonEventHandle(this);
 	});
 	
+	$('.freqButton').on('mouseup', function() {
+
+		soloMouseRunning = false;
+		// implement stopTone();
+	});
+	
+	$('.freqButton').on('hover', function() {
+
+		if (soloMouseRunning == true) {
+			freqButtonEventHandle(this);
+		}
+	});
+
+
 	/**
 	 * Keyboard Player
 	 * 
@@ -372,10 +450,15 @@ $(document).ready(function () {
 	 */
 	$(document).on('keydown', function(event) {
 
+		//console.log(event.which);
 		if (!event) event = window.event;
 		if (keyboardCollection.item(currentKeyboard)[ kbEventNumber[event.which] ] != -1) {
 			
-			$("#"+keyboardCollection.item(currentKeyboard)[ kbEventNumber[event.which]  ]).trigger('click');	
+			var elem = $("#"+keyboardCollection.item(currentKeyboard)[ kbEventNumber[event.which]  ]);
+			//console.log(elem);
+			$(elem).trigger('mousedown')
+				   .trigger('mouseup');
+			//$("#"+keyboardCollection.item(currentKeyboard)[ kbEventNumber[event.which]  ]).trigger('mousedown');	
 		}
 
 		return;
@@ -421,9 +504,218 @@ $(document).ready(function () {
 		
 		var pl = plucked.length + 1;
 		for (var i = 1; i <= pl; i++) {
-			$(plucked[i]).trigger('click');
+			console.log(plucked[i]);
+			playTone( $(plucked[i]).attr('id') ) ;
+			//$(plucked[i]).trigger('click');
 		}
 	});
+
+
+	/****** Synth Knob Events ******/
+
+	$('.knob').on('hover', function() {
+
+		switch ( $(this).attr('id')) {
+
+			case "filterFreq":
+				//console.log('filtering');
+			break;
+			/*
+			case "volume":
+				acDecay = $(this).val();
+			break;
+			/**/
+			case "eg_a":
+				acAttack = $(this).val();
+			break;
+			case "eg_r":
+				acDecay = $(this).val();
+			break;
+		}
+	});
+
+	/*
+	$('.knob #glideTime').on('change', function() {
+		
+		webSynth.vco1.set_glide_time( $(this).val() );
+		webSynth.vco2.set_glide_time( $(this).val() );
+		
+	}).appendTo('#synthesisControls');
+
+	$('.knob #glideToggle').on('change', function(){
+		
+		webSynth.vco1.set_glide_on($(this).val());
+		webSynth.vco2.set_glide_on($(this).val());
+
+	}).appendTo('#synthesisControls');
+
+	$('.knob #vco1pitch').on('change', function() {
+		change: (function() { webSynth.vco1.set_pitch($(this).val()); })
+	}).appendTo('#synthesisControls');
+
+	$('.knob #vco2pitch').on('change', function() {
+		change: (function() { webSynth.vco2.set_pitch($(this).val()); })
+	}).appendTo('#synthesisControls');
+	webSynth.vco2.set_pitch(0);
+
+	$('.knob #vco1fine').on('change', function() {
+		change: (function() { webSynth.vco1.set_fine($(this).val()); })
+	}).appendTo('#synthesisControls');
+
+	$('.knob #vco2fine').on('change', function() {
+		change: (function() { webSynth.vco2.set_fine($(this).val()); })
+	}).appendTo('#synthesisControls');
+
+	$('.knob #vco1wave').on('change', function() {
+		change: (function() { webSynth.vco1.set_wave($(this).val()); })
+	}).appendTo('#synthesisControls');
+
+	$('.knob #vco2wave').on('change', function() {
+		change: (function() { webSynth.vco2.set_wave($(this).val()); })
+	}).appendTo('#synthesisControls');
+
+
+	$('.knob #vco1gain').on('change', function() {
+		change: (function() { webSynth.vco1.set_gain($(this).val()); })
+	}).appendTo('#synthesisControls');
+
+	$('.knob #vco1toggle').on('change', function() {
+		click: (function() { webSynth.vco1.set_on($(this).val()); } )
+	}).appendTo('#synthesisControls');
+
+	$('.knob #vco2gain').on('change', function() {
+		change: (function() { webSynth.vco2.set_gain($(this).val()); } )
+	}).appendTo('#synthesisControls');
+
+	$('.knob #vco2toggle').on('change', function() {
+		click: (function() { webSynth.vco2.set_on($(this).val());} )
+	}).appendTo('#synthesisControls');
+
+	$('.knob #filterFreq').on('change', function() {
+		change: (function() { webSynth.filter.set_freq($(this).val());} )
+	}).appendTo('#synthesisControls');
+
+	$('.knob #resonance').on('change', function() {
+		change: (function() { webSynth.filter.set_q($(this).val()); } )
+	}).appendTo('#synthesisControls');
+
+	$('.knob #envAmount').on('change', function() {
+		change: (function() { webSynth.filter.set_amount($(this).val()); } )
+	}).appendTo('#synthesisControls');
+
+	$('.knob #feg_a').on('change', function() {
+		change: (function() { webSynth.feg.set_a($(this).val()); } )
+	}).appendTo('#synthesisControls');
+
+	$('.knob #feg_d').on('change', function() {
+		change: (function() { 
+			webSynth.feg.set_d($(this).val()); 
+			//webSynth.feg.set_r($(this).val());
+		})
+	}).appendTo('#synthesisControls');
+
+	$('.knob #feg_s').on('change', function() {
+		change: (function() { webSynth.feg.set_s($(this).val()); } )
+	}).appendTo('#synthesisControls');
+
+	$('.knob #feg_r').on('change', function() {
+		change: (function() { 
+			//webSynth.feg.set_d($(this).val()); 
+			webSynth.feg.set_r($(this).val());
+		})
+	}).appendTo('#synthesisControls');
+
+	$('.knob #eg_a').on('change', function() {
+		change: (function() { webSynth.eg.set_a($(this).val()); } )
+	}).appendTo('#synthesisControls');
+
+	$('.knob #eg_d').on('change', function() {
+		change: (function() { 
+			webSynth.eg.set_d($(this).val()); 
+			//webSynth.eg.set_r($(this).val());
+		})
+	}).appendTo('#synthesisControls');
+
+	$('.knob #eg_s').on('change', function() {
+		change: (function() { webSynth.eg.set_s($(this).val()); } )
+	}).appendTo('#synthesisControls');
+	
+	$('#eg_r').on('change', function() {
+		change: (function() {
+			console.log('success');
+			acDecay = $(this).val();
+			//webSynth.eg.set_d($(this).val()); 
+			//webSynth.eg.set_r($(this).val());
+		})
+	}); //.appendTo('#synthesisControls');
+	
+	$('.knob #volume').on('change', function() {
+		change: (function() { webSynth.volume.set($(this).val()); } )
+	}).appendTo('#synthesisControls');
+
+	$('.knob #delay').on('change', function() {
+		//wet gain val
+		change: (function() { webSynth.delay.set($(this).val()); } )
+	}).appendTo('#synthesisControls');
+
+	/**/
+
+
+
+
+	/**
+	 * To be developed Knob Lights, KnobKnob, deprecating maybe
+	 * @type {Array}
+	 */
+	var colors = [
+        '26e000','2fe300','37e700','45ea00','51ef00',
+        '61f800','6bfb00','77ff02','80ff05','8cff09',
+        '93ff0b','9eff09','a9ff07','c2ff03','d7ff07',
+        'f2ff0a','fff30a','ffdc09','ffce0a','ffc30a',
+        'ffb509','ffa808','ff9908','ff8607','ff7005',
+        'ff5f04','ff4f03','f83a00','ee2b00','e52000'
+    ];
+
+    var rad2deg = 180/Math.PI;
+    var deg = 0;
+    var bars = $('#bars');
+
+    for(var i = 0; i < colors.length; i++){
+
+        deg = i * 12;
+
+        $('<div class="colorBar">').css({
+            backgroundColor: '#'+colors[i],
+            transform:'rotate('+deg+'deg)',
+            top: -Math.sin(deg/rad2deg)*80+100,
+            left: Math.cos((180 - deg)/rad2deg)*80+100,
+        }).appendTo(bars);
+    }
+
+    var colorBars = bars.find('.colorBar');
+    var numBars = 0, lastNum = -1;
+
+	/*
+	$('#synthesisControls .knob').knobKnob({
+		//console.log(this);
+
+	    snap : 10,
+	    value: 154,
+	    turn : function(ratio){
+	        // Do what you want here. Ratio moves from 0 to 1
+	        // relative to the knob rotation. 0 - off, 1 - max
+	        
+	        numBars = Math.round(colorBars.length*ratio);
+            // Update the dom only when the number of active bars
+            // changes, instead of on every move
+            if(numBars == lastNum){
+                return false;
+            }
+            lastNum = numBars;
+            colorBars.removeClass('active').slice(0, numBars).addClass('active');
+	    }
+	});
+	/* END KnobKnob */
 
 	/**
 	 * Begin algorithmical play
@@ -464,7 +756,3 @@ $(document).ready(function () {
 	
 
 });
-
-
-
-
