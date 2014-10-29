ab592e566327f05cb0eacf5ba96486dba37c13cd
Miika Luolajan-Mikkola <miika@luminescent-music.com>
2014-10-29 12:01:48 +0200

    Meaningful examples

--- js/temperings.js
+++ js/temperings.js
@@ -1,6 +1,6 @@
 
 var tprColl = new Collection();
-tprColl.add('Origo [555]', 'function');
+tprColl.add('Lo Shu Square', 'function');
 tprColl.add('Stockhausen 5:1 (tba)', 'function');
 tprColl.add('Pythagorean (tba)', 'function');
 tprColl.add('Kirnberger III (tba)', 'function');
