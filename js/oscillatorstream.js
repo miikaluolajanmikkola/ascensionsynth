6756c0d6c96387d923d7a1d751d1917eff26aebc
Miika Luolajan-Mikkola <miika@luminescent-music.com>
2014-10-29 17:36:40 +0200

    Cleaning up tests, amplitude seems to work

--- js/oscillatorstream.js
+++ js/oscillatorstream.js
@@ -3,24 +3,27 @@
 //Web Audio Context
 //https://developer.mozilla.org/en-US/docs/Web/API/AudioContext.createBufferSource
 
-var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-if( audioCtx == null ) alert('Your browser does not support Web Audio Api. Performance will be crap now, sorry');
-var channels = 2;
+//var AC = new (window.AudioContext || window.webkitAudioContext)();
+//if( AC == null ) alert('Your browser does not support Web Audio Api.');
+
+// Remember, BiquadFilter takes mono input
+var channels = 1;
 // Create an empty two second stereo buffer at the
 // sample rate of the AudioContext
-var frameCount    = audioCtx.sampleRate;
-var myArrayBuffer = audioCtx.createBuffer(2, frameCount, audioCtx.sampleRate);
+var frameCount    = 8192; //AC.sampleRate;
+
+// Is it dangerous to call this when the function further below wants to write straight to the node?
+var acBuffer = AC.createBuffer(channels, frameCount, AC.sampleRate);
 
 
 function bufferWaveStream(samples) {
     
-    //NOTE: This seems to not implement the Web Audio ScriptProcessorNode, which should be tested too.
+    //NOTE: This seems to not implement the Web Audio ScriptProcessorNode?
     //console.log(samples);
     for (var channel = 0; channel < channels; channel++) {
         
-        // This gives us the actual ArrayBuffer that contains the data
-        //var nowBuffering = myArrayBuffer.getChannelData(channel);
-        var nowBuffering = myArrayBuffer.getChannelData(channel);
+        //var nowBuffering = acBuffer.getChannelData(channel);
+        var nowBuffering = acBuffer.getChannelData(channel);
         for (var i = 0; i < frameCount; i++) {
             //nowBuffering[i] = bufferRuntimeCallback('fmod');
             //nowBuffering[i] = sin( 2*PI*i );
@@ -28,22 +31,27 @@ function bufferWaveStream(samples) {
         }
     }
 
-    // Get an AudioBufferSourceNode.
-    // This is the AudioNode to use when we want to play an AudioBuffer
-    var source = audioCtx.createBufferSource();
+    //var delay = new FX_Delay(AC);
 
-    //var gaynode = audioCtx.createGain();
-    source.connect(gaynode);
+    var gaynode = AC.createGain();
+    gaynode.gain.value = 10;
+    // Get an AudioBufferSourceNode. This is the AudioNode to use when we want to play an AudioBuffer
+    var source = AC.createBufferSource();
+
+    //gaynode.connect(source);
+    
     // set the buffer in the AudioBufferSourceNode
-    source.buffer = myArrayBuffer;
+    source.buffer = acBuffer;
     // connect the AudioBufferSourceNode to the
     // destination so we can hear the sound
-    source.connect(audioCtx.destination);
+    source.connect(AC.destination);
 
-    //gaynode.gain.value = 0.01;
+    
 
     // start the source playing
     source.start();
+    
+    return;
 }
 
 
