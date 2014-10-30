a546c3ba41052851335e11b7a40f611845495288
Miika Luolajan-Mikkola <miika@luminescent-music.com>
2014-10-30 09:50:47 +0200

    Comments, cleanup

--- js/oscillatorstream.js
+++ js/oscillatorstream.js
@@ -1,20 +1,14 @@
-//Oscillator Streaming
+//Oscillator Streaming with algorithms from waves.js
 
-//Web Audio Context
 //https://developer.mozilla.org/en-US/docs/Web/API/AudioContext.createBufferSource
 
-//var AC = new (window.AudioContext || window.webkitAudioContext)();
-//if( AC == null ) alert('Your browser does not support Web Audio Api.');
-
-// Remember, BiquadFilter takes mono input
+/* Defined in ScriptProcessorNode.js
+var AC = new (window.AudioContext || window.webkitAudioContext)();
+if( AC == null ) alert('Your browser does not support Web Audio Api.');
 var channels = 1;
-// Create an empty two second stereo buffer at the
-// sample rate of the AudioContext
 var frameCount    = 8192; //AC.sampleRate;
-
-// Is it dangerous to call this when the function further below wants to write straight to the node?
 var acBuffer = AC.createBuffer(channels, frameCount, AC.sampleRate);
-
+/**/
 
 function bufferWaveStream(samples) {
     
@@ -34,7 +28,7 @@ function bufferWaveStream(samples) {
     //var delay = new FX_Delay(AC);
 
     var gaynode = AC.createGain();
-    gaynode.gain.value = 10;
+    
     // Get an AudioBufferSourceNode. This is the AudioNode to use when we want to play an AudioBuffer
     var source = AC.createBufferSource();
 
@@ -46,7 +40,7 @@ function bufferWaveStream(samples) {
     // destination so we can hear the sound
     source.connect(AC.destination);
 
-    
+    gaynode.gain.value = 10;
 
     // start the source playing
     source.start();
