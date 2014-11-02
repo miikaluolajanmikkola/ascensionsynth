56dd431d126045b04d7a156bc0bed2d57de74561
Miika Luolajan-Mikkola <miika@luminescent-music.com>
2014-11-02 05:17:45 +0200

    unused hidden

--- js/temperings.js
+++ js/temperings.js
@@ -1,9 +1,9 @@
 
 var tprColl = new Collection();
-tprColl.add('Lo Shu Square', 'function');
-tprColl.add('Stockhausen 5:1 (tba)', 'function');
-tprColl.add('Pythagorean (tba)', 'function');
-tprColl.add('Kirnberger III (tba)', 'function');
+tprColl.add('Lo Shu Fundamental', 'function');
+//tprColl.add('Stockhausen 5:1 (tba)', 'function');
+//tprColl.add('Pythagorean (tba)', 'function');
+//tprColl.add('Kirnberger III (tba)', 'function');
 
 function getTemperingCollection() {
 	return tprColl;
